fd85c3f8f5e04137a54d7a75f9bb8c59c8998a4f
Carlos <85038929+Carlos-A-P@users.noreply.github.com>
2021-07-25 09:56:58 -0700

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+function togglePopup(){
+    // this will add the class "active" to my element with the id popup-1 which replaces display hidden with display block and makes my content transition in when scaled back to regular size"
+    document.getElementById("popup-1").classList.toggle("active");
+}
